ba7a0e981443a6f7632053d351db021709959083
George Higbie <gehigbie@gmail.com>
2017-03-10 16:50:49 -0700

    added fragment transaction

--- app/src/main/java/com/geogehigbie/listjsonparse/activities/MainActivity.java
+++ app/src/main/java/com/geogehigbie/listjsonparse/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package com.geogehigbie.listjsonparse.activities;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void centerActionBar(){
+        android.app.ActionBar actionBar = getActionBar();
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); //this is a soft error
+        getSupportActionBar().setCustomView(R.layout.actionbar);
+
+    }
+
+
     public void setButtonListner(){
         Button button = (Button) findViewById(R.id.button_open);
         button.setOnClickListener(new View.OnClickListener() {
@@ -27,8 +37,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-
+                fragmentTransaction.add(R.id.fragment_container, new ListFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
     }
