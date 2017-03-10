4e39e8d202aa438aef43d81e86c0f9876f1509ee
George Higbie <gehigbie@gmail.com>
2017-03-10 15:51:00 -0700

    added button to layout

--- app/src/main/java/com/geogehigbie/listjsonparse/MainActivity.java
+++ app/src/main/java/com/geogehigbie/listjsonparse/MainActivity.java
@@ -1,7 +1,9 @@
 package com.geogehigbie.listjsonparse;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setButtonListner();
+
+    }
+
+    public void setButtonListner(){
+        Button button = (Button) findViewById(R.id.button_open);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
